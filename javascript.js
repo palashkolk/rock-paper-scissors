4dfafa8fe405146fdb5699b2e8a7c71d7104551a
palashkolk <203395566+palashkolk@users.noreply.github.com>
2025-03-30 17:22:01 +0530

    Adds playRound function to play a 5 round game
    Takes care of the requirement to play five rounds
    of rock-paper-scissors game. Made use of loops.

--- javascript.js
+++ javascript.js
@@ -5,52 +5,65 @@ function getComputerChoice(){
         return "rock";
     }else if (randNumber>33.3 && randNumber<=66.6){
         return "paper";
-
     }else {
         return "scissors";
     }
 }
-// console.log(getComputerChoice())
+
 function getHumanChoice(){
     const humanChoice=prompt("What's your choice? rock, paper or scissors?");
     return humanChoice;
 }
-// console.log(getComputerChoice())
-// console.log(getHumanChoice())
 
-let humanScore=0;
-let computerScore=0;
+function playGame(){
+    let humanScore=0;
+    let computerScore=0;
 
-function playRound(humanChoice, computerChoice){
-    const humanChoiceLowerCase=humanChoice.toLowerCase();
-    // console.log(humanChoiceLowerCase);
-    if(humanChoiceLowerCase!=computerChoice){
-        if(humanChoiceLowerCase=="rock" && computerChoice=="paper"){
-            console.log(`You loose! ${computerChoice} beats ${humanChoiceLowerCase}!`);
-            computerScore+=1;
+    function playRound(humanChoice, computerChoice){
+        const humanChoiceLowerCase=humanChoice.toLowerCase();
+        // console.log(humanChoiceLowerCase);
+        if(humanChoiceLowerCase!=computerChoice){
+            if(humanChoiceLowerCase=="rock" && computerChoice=="paper"){
+                console.log(`You loose! ${computerChoice} beats ${humanChoiceLowerCase}!`);
+                computerScore+=1;
 
-        }else if(humanChoiceLowerCase=="scissors" && computerChoice=="rock"){
-            console.log(`You loose! ${computerChoice} beats ${humanChoiceLowerCase}!`);
-            computerScore+=1;
+            }else if(humanChoiceLowerCase=="scissors" && computerChoice=="rock"){
+                console.log(`You loose! ${computerChoice} beats ${humanChoiceLowerCase}!`);
+                computerScore+=1;
 
-        }else if(humanChoiceLowerCase=="paper" && computerChoice=="scissors"){
-            console.log(`You loose! ${computerChoice} beats ${humanChoiceLowerCase}!`);
-            computerScore+=1;
+            }else if(humanChoiceLowerCase=="paper" && computerChoice=="scissors"){
+                console.log(`You loose! ${computerChoice} beats ${humanChoiceLowerCase}!`);
+                computerScore+=1;
 
-        }else {
-            console.log(`You win! ${humanChoiceLowerCase} beats ${computerChoice}!`);
-            humanScore+=1;
+            }else {
+                console.log(`You win! ${humanChoiceLowerCase} beats ${computerChoice}!`);
+                humanScore+=1;
+            }
+
+        }else{
+            console.log("You tied!");
         }
+        
 
-    }else{
-        console.log("You tied!");
     }
-    
+    let humanSelection;
+    let computerSelection;
+    for(let i=0;i<5;i++){    
+            humanSelection=getHumanChoice();
+            computerSelection=getComputerChoice();    
+            playRound(humanSelection, computerSelection)
+
+    }
+
+console.log(`Your score is ${humanScore} and computer score is ${computerScore}`)
+if(computerScore>humanScore){
+    console.log("Computer won");
+}else if (humanScore>computerScore){
+    console.log("You win");
+}else{
+    console.log("You tied!")
+}
 
 }
 
-
-const humanSelection=getHumanChoice();
-const computerSelection=getComputerChoice();
-// console.log(humanSelection, computerSelection);
-playRound(humanSelection, computerSelection)
+playGame()
