a71b1044d0ca9f6b07486f934ec4c755b1fde001
palashkolk <203395566+palashkolk@users.noreply.github.com>
2025-04-05 20:23:12 +0530

    Creates buttons and displays scores and status
    Done as part of the javascript exercise of the
    ODIN project. Needs further improvement.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,26 @@
+let btnArray=[];
+choiceList=['rock', 'paper', 'scissors'];
+for (let i=0;i<3;i++){
+    btnArray.push(document.createElement("button"));
+    btnArray[i].textContent=choiceList[i];
+}
+
+let div=document.querySelector("#container");
+for (let btn of btnArray){
+    div.appendChild(btn);
+}
+div.addEventListener('click', (event)=>{
+    let target =event.target;
+    playRound(getHumanChoice(target.textContent), getComputerChoice());
+});
+let para1 =document.createElement("p");
+let para2 =document.createElement("p");
+para2.textContent="Let's play";
+div.appendChild(para1)
+div.appendChild(para2);
+
+
+
 function getComputerChoice(){
     const randNumber=Math.random()*100+1;
 
@@ -10,62 +33,97 @@ function getComputerChoice(){
     }
 }
 
-function getHumanChoice(){
-    const humanChoice=prompt("What's your choice? rock, paper or scissors?");
+function getHumanChoice(choice){
+    const humanChoice=choice;
     return humanChoice;
 }
+let humanScore=0;
+let computerScore=0;
+function playRound(humanChoice, computerChoice){
+    const humanChoiceLowerCase=humanChoice.toLowerCase();
+    if(humanChoiceLowerCase!=computerChoice){
+        if(humanChoiceLowerCase=="rock" && computerChoice=="paper"){
+            para1.textContent=`You loose! ${computerChoice} beats ${humanChoiceLowerCase}!`;
+            console.log(`You loose! ${computerChoice} beats ${humanChoiceLowerCase}!`);
+            computerScore+=1;
 
-function playGame(){
-    let humanScore=0;
-    let computerScore=0;
+        }else if(humanChoiceLowerCase=="scissors" && computerChoice=="rock"){
+            para1.textContent=`You loose! ${computerChoice} beats ${humanChoiceLowerCase}!`;
+            console.log(`You loose! ${computerChoice} beats ${humanChoiceLowerCase}!`);
+            computerScore+=1;
 
-    function playRound(humanChoice, computerChoice){
-        const humanChoiceLowerCase=humanChoice.toLowerCase();
-        // console.log(humanChoiceLowerCase);
-        if(humanChoiceLowerCase!=computerChoice){
-            if(humanChoiceLowerCase=="rock" && computerChoice=="paper"){
-                console.log(`You loose! ${computerChoice} beats ${humanChoiceLowerCase}!`);
-                computerScore+=1;
+        }else if(humanChoiceLowerCase=="paper" && computerChoice=="scissors"){
+            para1.textContent=`You loose! ${computerChoice} beats ${humanChoiceLowerCase}!`;
+            console.log(`You loose! ${computerChoice} beats ${humanChoiceLowerCase}!`);
+            computerScore+=1;
 
-            }else if(humanChoiceLowerCase=="scissors" && computerChoice=="rock"){
-                console.log(`You loose! ${computerChoice} beats ${humanChoiceLowerCase}!`);
-                computerScore+=1;
-
-            }else if(humanChoiceLowerCase=="paper" && computerChoice=="scissors"){
-                console.log(`You loose! ${computerChoice} beats ${humanChoiceLowerCase}!`);
-                computerScore+=1;
-
-            }else {
-                console.log(`You win! ${humanChoiceLowerCase} beats ${computerChoice}!`);
-                humanScore+=1;
-            }
-
-        }else{
-            console.log("You tied!");
+        }else {
+            para1.textContent=`You win! ${humanChoiceLowerCase} beats ${computerChoice}!`;
+            console.log(`You win! ${humanChoiceLowerCase} beats ${computerChoice}!`);
+            humanScore+=1;
         }
-        
 
+    }else{
+        para1.textContent=`You tied!`;
+        console.log("You tied!");
     }
-    let humanSelection;
-    let computerSelection;
-    for(let i=0;i<5;i++){    
-            humanSelection=getHumanChoice();
-            computerSelection=getComputerChoice();    
-            playRound(humanSelection, computerSelection)
+    console.log(humanScore, computerScore);
+    para2.textContent=`human score is: ${humanScore} and computer score is ${computerScore}`;
 
-    }
-    declareResult();
-    
-    function declareResult(){
-        console.log(`Your score is ${humanScore} and computer score is ${computerScore}`)
-        if(computerScore>humanScore){
-            console.log("Final result: Computer won");
-        }else if (humanScore>computerScore){
-            console.log("Final result: You win");
-        }else{
-            console.log("Final result: Result tied!")
-        }
-    }
 }
 
-playGame()
+
+// function playGame(){
+//     let humanScore=0;
+//     let computerScore=0;
+
+//     function playRound(humanChoice, computerChoice){
+//         const humanChoiceLowerCase=humanChoice.toLowerCase();
+//         // console.log(humanChoiceLowerCase);
+//         if(humanChoiceLowerCase!=computerChoice){
+//             if(humanChoiceLowerCase=="rock" && computerChoice=="paper"){
+//                 console.log(`You loose! ${computerChoice} beats ${humanChoiceLowerCase}!`);
+//                 computerScore+=1;
+
+//             }else if(humanChoiceLowerCase=="scissors" && computerChoice=="rock"){
+//                 console.log(`You loose! ${computerChoice} beats ${humanChoiceLowerCase}!`);
+//                 computerScore+=1;
+
+//             }else if(humanChoiceLowerCase=="paper" && computerChoice=="scissors"){
+//                 console.log(`You loose! ${computerChoice} beats ${humanChoiceLowerCase}!`);
+//                 computerScore+=1;
+
+//             }else {
+//                 console.log(`You win! ${humanChoiceLowerCase} beats ${computerChoice}!`);
+//                 humanScore+=1;
+//             }
+
+//         }else{
+//             console.log("You tied!");
+//         }
+        
+
+//     }
+//     let humanSelection;
+//     let computerSelection;
+//     // for(let i=0;i<5;i++){    
+//     //         humanSelection=getHumanChoice();
+//     //         computerSelection=getComputerChoice();    
+//     //         playRound(humanSelection, computerSelection)
+
+//     // }
+//     declareResult();
+    
+//     function declareResult(){
+//         console.log(`Your score is ${humanScore} and computer score is ${computerScore}`)
+//         if(computerScore>humanScore){
+//             console.log("Final result: Computer won");
+//         }else if (humanScore>computerScore){
+//             console.log("Final result: You win");
+//         }else{
+//             console.log("Final result: Result tied!")
+//         }
+//     }
+// }
+
+// playGame()
