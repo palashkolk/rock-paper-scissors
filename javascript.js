556d88c987f91458fc2d5f59cb6b53798512f564
palashkolk <203395566+palashkolk@users.noreply.github.com>
2025-04-06 14:20:50 +0530

    Adds CSS to the elemens via javascript
    Done as part of the requirement of the ODIN
    project.

--- javascript.js
+++ javascript.js
@@ -1,31 +1,57 @@
 let btnArray=[];
-choiceList=['rock', 'paper', 'scissors'];
-for (let i=0;i<3;i++){
+let choiceList=['rock', 'paper', 'scissors'];
+let numberOfChoice=choiceList.length;
+for (let i=0;i<numberOfChoice;i++){
     btnArray.push(document.createElement("button"));
     btnArray[i].textContent=choiceList[i];
-}
+    btnArray[i].style.minWidth="100px";
+    btnArray[i].style.minHeight="50px";
+    btnArray[i].style.borderRadius="8px";
+    btnArray[i].style.fontSize="22px";
 
+}
 let div=document.querySelector("#container");
+let div2=document.querySelector("#announcements");
 for (let btn of btnArray){
     div.appendChild(btn);
 }
-let para1 =document.createElement("p");
-let para2 =document.createElement("p");
-let para3 =document.createElement("p");
-para2.textContent="Let's play";
-para3.textContent=`Computer choice = , Human choice = `;
-div.appendChild(para3);
-div.appendChild(para1)
-div.appendChild(para2);
 
-div.addEventListener('click', (event)=>{
+div.style.display="flex";
+div.style.justifyContent="center";
+div.style.gap="20px";
+div.style.padding="10px";
+
+// div2.style.backgroundColor="grey";
+div2.style.color="black";
+div2.style.textAlign="center";
+div2.style.fontSize="30px";
+let paraShowResult =document.createElement("p");
+let paraShowScore =document.createElement("p");
+let paraShowChoice =document.createElement("p");
+let paraArray=[paraShowChoice, paraShowResult, paraShowScore];
+
+for (let para of paraArray){
+    div2.appendChild(para);
+}
+
+initialChoice=`Computer choice = , Human choice = `;
+initialResult="Let's play; Click above on your choice!";
+initialScore="All score set to zero";
+paraShowChoice.textContent=initialChoice;
+paraShowResult.textContent=initialResult;
+paraShowScore.textContent=initialScore;
+
+
+let body=document.querySelector("body");
+body.addEventListener('click', (event)=>{
     let target =event.target;
-    if(target.textContent=="rock"|| target.textContent=="paper"||target.textContent=="scissors"){
-        playRound(getHumanChoice(target.textContent), getComputerChoice());
-        para2.style.backgroundColor='white';
+    let humanChoice=target.textContent;
+    if(humanChoice=="rock"|| humanChoice=="paper"||humanChoice=="scissors"){
+        playRound(getHumanChoice(humanChoice), getComputerChoice());
+        paraShowScore.style.backgroundColor='white';
     }else {
-        para2.textContent="Click on the button, please!";
-        para2.style.backgroundColor='red';
+        paraShowScore.textContent="Click on the button, please!";
+        paraShowScore.style.backgroundColor='red';
     }
 });
 
@@ -48,41 +74,41 @@ let humanScore=0;
 let computerScore=0;
 function playRound(humanChoice, computerChoice){
     const humanChoiceLowerCase=humanChoice.toLowerCase();
-    para3.textContent=`Computer choice = ${computerChoice}, human choice= ${humanChoiceLowerCase}!`;
+    paraShowChoice.textContent=`Computer choice = ${computerChoice}, human choice= ${humanChoiceLowerCase}!`;
     if(humanChoiceLowerCase!=computerChoice){
         if(humanChoiceLowerCase=="rock" && computerChoice=="paper"){
-            para1.textContent=`You loose! ${computerChoice} beats ${humanChoiceLowerCase}!`;
+            paraShowResult.textContent=`You loose! ${computerChoice} beats ${humanChoiceLowerCase}!`;
             console.log(`You loose! ${computerChoice} beats ${humanChoiceLowerCase}!`);
             computerScore+=1;
 
         }else if(humanChoiceLowerCase=="scissors" && computerChoice=="rock"){
-            para1.textContent=`You loose! ${computerChoice} beats ${humanChoiceLowerCase}!`;
+            paraShowResult.textContent=`You loose! ${computerChoice} beats ${humanChoiceLowerCase}!`;
             console.log(`You loose! ${computerChoice} beats ${humanChoiceLowerCase}!`);
             computerScore+=1;
 
         }else if(humanChoiceLowerCase=="paper" && computerChoice=="scissors"){
-            para1.textContent=`You loose! ${computerChoice} beats ${humanChoiceLowerCase}!`;
+            paraShowResult.textContent=`You loose! ${computerChoice} beats ${humanChoiceLowerCase}!`;
             console.log(`You loose! ${computerChoice} beats ${humanChoiceLowerCase}!`);
             computerScore+=1;
 
         }else {
-            para1.textContent=`You win! ${humanChoiceLowerCase} beats ${computerChoice}!`;
+            paraShowResult.textContent=`You win! ${humanChoiceLowerCase} beats ${computerChoice}!`;
             console.log(`You win! ${humanChoiceLowerCase} beats ${computerChoice}!`);
             humanScore+=1;
         }
 
     }else{
-        para1.textContent=`You tied!`;
+        paraShowResult.textContent=`You tied!`;
         console.log("You tied!");
     }
     console.log(humanScore, computerScore);
-    para2.textContent=`human score is: ${humanScore} and computer score is ${computerScore}`;
-    if(humanScore==5 ){
-        para2.textContent =`We have a winner! The winner is Human`;
-        humanScore=0;
-        computerScore=0
-    }else if(computerScore==5){
-        para2.textContent =`We have a winner! The winner is Computer`;
+    paraShowScore.textContent=`human score is: ${humanScore} and computer score is ${computerScore}`;
+    if(humanScore==5 || computerScore==5){
+        if(humanScore==5 ){
+            paraShowScore.textContent =`We have a winner! The winner is Human`;
+        }else if(computerScore==5){
+            paraShowScore.textContent =`We have a winner! The winner is Computer`;
+        }
         humanScore=0;
         computerScore=0
     }
